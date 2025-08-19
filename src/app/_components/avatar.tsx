7e64a13d6011234d6fbfc5f2597f7b9944e57631
masaki0087 <pcmv2846@yahoo.co.jp>
2025-08-19 12:06:27 +0900

    Avatarコンポーネントにbasepathを設定

--- src/app/_components/avatar.tsx
+++ src/app/_components/avatar.tsx
@@ -1,3 +1,6 @@
+ import nextConfig from "../../../next.config.mjs";
+ const BASE_PATH = nextConfig.basePath || "";
+
 type Props = {
   name: string;
   picture: string;
@@ -6,7 +9,7 @@ type Props = {
 const Avatar = ({ name, picture }: Props) => {
   return (
     <div className="flex items-center">
-      <img src={picture} className="w-12 h-12 rounded-full mr-4" alt={name} />
+      <img src={`${BASE_PATH}${picture}`} className="w-12 h-12 rounded-full mr-4" alt={name} />
       <div className="text-xl font-bold">{name}</div>
     </div>
   );
