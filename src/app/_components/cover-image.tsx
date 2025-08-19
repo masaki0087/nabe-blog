ae1e2bb98a857ac3d74b8d1b6bce2e7601b327c9
masaki0087 <pcmv2846@yahoo.co.jp>
2025-08-19 11:55:42 +0900

    basepathを設定

--- src/app/_components/cover-image.tsx
+++ src/app/_components/cover-image.tsx
@@ -1,6 +1,8 @@
 import cn from "classnames";
 import Link from "next/link";
 import Image from "next/image";
+import nextConfig from "../../../next.config.mjs";
+const BASE_PATH = nextConfig.basePath || "";
 
 type Props = {
   title: string;
@@ -11,7 +13,7 @@ type Props = {
 const CoverImage = ({ title, src, slug }: Props) => {
   const image = (
     <Image
-      src={src}
+      src={`${BASE_PATH}${src}`}
       alt={`Cover Image for ${title}`}
       className={cn("shadow-sm w-full", {
         "hover:shadow-lg transition-shadow duration-200": slug,
